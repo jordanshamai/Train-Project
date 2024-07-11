185491e1e987ab9f7e698a5461b9356430a32a1d
Caleb Angelito <calebangelito@gmail.com>
2024-07-11 19:29:43 -0400

    add comment in cs336project_customer.sql

--- cs336project_customer.sql
+++ cs336project_customer.sql
@@ -6,6 +6,8 @@ USE `cs336project`;
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
+-- hello
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
